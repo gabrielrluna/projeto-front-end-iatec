9c51757b9379de6bf7da4b66a6a0ae55c8119f28
Gabriel Luna <lunagabri@gmail.com>
2023-06-21 13:14:22 -0300

    Programação

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-const seta = $('#seta');
+var listaProdutos = [];
 
-function irParaCadastro(){
-    console.log("Teste");
+
+function cadastrar() {
+    var produto = $("#produto").val();
+    var marca = $("#marca").val();
+    var peso = $("#peso").val();
+    var setor = $("#setor").val();
+
+    var validacao = validFormulario(produto, marca, peso, setor);
+        if (!validacao){
+            alert("Preencha todos os dados solicitados");
+            return;
+        }
+
+        let infProduto = {produto, marca, peso, setor}
+        listaProdutos.push(infProduto);
+        AdicionarProduto(infProduto);
+    }
+
+function AdicionarProduto(infProduto) {
+    var elementoHtml = document.getElementById("lista-produtos");
+    var novoProduto =`
+        <div>
+            <p>Produto: ${infProduto.produto}</p>
+            <p>Marca: ${infProduto.marca}</p>
+            <p>Peso: ${infProduto.peso}</p>
+            <p>Setor: ${infProduto.setor}</p>
+
+            <button onclick='ExcluirProduto(this)' class="btn btn-danger">
+            <i class="fa-solid fa-xmark"></i>
+            </button>
+        </div>`;
+    elementoHtml.insertAdjacentHTML("beforeend",novoProduto);
+}
+
+function ExcluirProduto(element) {
+    element.parentNode.remove();
+    
+}
+
+function validFormulario(produto, marca, peso, setor) {
+    if (!produto || !marca || !peso || !setor){
+        return false;
+    } return true;
+    
 }
