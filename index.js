b4ecaf15b22bbc19798d701bb005ec65e3449a60
Gabriel Luna <lunagabri@gmail.com>
2023-06-23 15:23:45 -0300

    a

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function AdicionarProduto(infProduto) {
 
    
     var novoProduto =`
-    <div id="uniProduto" class="bg-warning m-1 p-3 col-3">
+    <div id="uniProduto" class="bg-warning m-1 p-3 col">
     <p><b>Produto:</b> ${infProduto.produto}</p>
     <p><b>Marca:</b> ${infProduto.marca}</p>
     <p><b>Peso (em kg):</b> ${infProduto.peso} kg</p>
@@ -50,11 +50,26 @@ function validFormulario(produto, marca, peso, setor) {
     
 }
 
-function QuantidadeProdutos(listaProdutos){
-var qProdutos = document.getElementById('qProdutos')
-let quantProdutos = document.createElement('div');
-quantProdutos.innerHTML = `<p>Quantidade de produtos cadastrados: ${listaProdutos.length}</p>`;
-qProdutos.appendChild(quantProdutos)
+
+function quantidadeProdutos(listaProdutos){
+
+    var qProdutos = document.getElementById("qProdutos");
+    var quantProdutos = document.createElement('div');
+    quantProdutos.innerHTML = `<p id="teste" class="text-center text-white"> Quantidade de produtos cadastrados: ${listaProdutos.length}</p>`;
+    qProdutos.appendChild(quantProdutos);
 
 }
 
+// Configurando modo noturno
+const pagina = document.querySelector ('body');
+const botao = pagina.querySelector('#noturno');
+
+botao.addEventListener('click', function(){
+    pagina.classList.toggle ('modo-noturno');
+
+    if (botao.textContent =='Ativar - Modo Noturno'){
+        botao.textContent = 'Desativar - Modo Noturno' ;
+} else {
+    botao.textContent = 'Ativar - Modo Noturno';
+}
+});
